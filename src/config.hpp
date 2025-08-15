fc66fb09ffafc1c5b76b02bc83363d7e92e59170
5C6F2F <5c6f2f@gmail.com>
2025-08-15 23:56:24 +0900

    feat: 測定輪用のダミーピンを追加

--- src/config.hpp
+++ src/config.hpp
@@ -3,9 +3,10 @@
 
 namespace DigitalOutPins
 {
-    constexpr PinName OMUNI_MOTOR1_DIR  = PB_10;     // オムニホイールDC1の方向
-    constexpr PinName OMUNI_MOTOR2_DIR  = PC_7 ;     // オムニホイールDC2の方向
-    constexpr PinName OMUNI_MOTOR3_DIR  = PB_4 ;     // オムニホイールDC3の方向
+    constexpr PinName OMNI_MOTOR1_DIR   = PB_10;     // オムニホイールDC1の方向
+    constexpr PinName OMNI_MOTOR2_DIR   = PC_7 ;     // オムニホイールDC2の方向
+    constexpr PinName OMNI_MOTOR3_DIR   = PB_4 ;     // オムニホイールDC3の方向
+    constexpr PinName OMNI_DUMMY_DIR    = PB_4 ;     // 測定輪用のダミー
 
     constexpr PinName PSEUDO_SERVO1_DIR = PB_9 ;     // 疑似サーボ用DC1の方向
     constexpr PinName PSEUDO_SERVO2_DIR = PB_6 ;     // 疑似サーボ用DC2の方向
@@ -16,9 +17,10 @@ namespace DigitalOutPins
 
 namespace PwmOutPins
 {
-    constexpr PinName OMUNI_MOTOR1_PWM  = PA_8 ;     // オムニホイールDC1のPWM (Tim1)
-    constexpr PinName OMUNI_MOTOR2_PWM  = PA_9 ;     // オムニホイールDC2のPWM (Tim1)
-    constexpr PinName OMUNI_MOTOR3_PWM  = PA_10;     // オムニホイールDC3のPWM (Tim1)
+    constexpr PinName OMNI_MOTOR1_PWM   = PA_8 ;     // オムニホイールDC1のPWM (Tim1)
+    constexpr PinName OMNI_MOTOR2_PWM   = PA_9 ;     // オムニホイールDC2のPWM (Tim1)
+    constexpr PinName OMNI_MOTOR3_PWM   = PA_10;     // オムニホイールDC3のPWM (Tim1)
+    constexpr PinName OMNI_DUMMY_PWM    = PA_10;     // 測定輪用のダミー
 
     constexpr PinName PSEUDO_SERVO1_PWM = PA_6 ;     // 疑似サーボ用DC1のPWM (Tim3)
     constexpr PinName PSEUDO_SERVO2_PWM = PA_7 ;     // 疑似サーボ用DC2のPWM (Tim3)
@@ -34,9 +36,9 @@ namespace InterruptInPins
     constexpr PinName MEASURING_ENCODER1_A      = PC_10;     // エンコーダー1：測定輪エンコーダ1のA相
     constexpr PinName MEASURING_ENCODER2_A      = PC_11;     // エンコーダー2：測定輪エンコーダ2のA相
 
-    constexpr PinName OMUNI_ENCODER1_A          = PC_2 ;     // エンコーダー3：オムニ用エンコーダー1のA相
-    constexpr PinName OMUNI_ENCODER2_A          = PC_0 ;     // エンコーダー4：オムニ用エンコーダー2のA相
-    constexpr PinName OMUNI_ENCODER3_A          = PC_9 ;     // エンコーダー5：オムニ用エンコーダー3のA相
+    constexpr PinName OMNI_ENCODER1_A           = PC_2 ;     // エンコーダー3：オムニ用エンコーダー1のA相
+    constexpr PinName OMNI_ENCODER2_A           = PC_0 ;     // エンコーダー4：オムニ用エンコーダー2のA相
+    constexpr PinName OMNI_ENCODER3_A           = PC_9 ;     // エンコーダー5：オムニ用エンコーダー3のA相
 
     constexpr PinName PSEUDO_SERVO_ENCODER1_A   = PC_8 ;     // エンコーダー6：疑似サーボ用エンコーダー1のA相
     constexpr PinName PSEUDO_SERVO_ENCODER2_A   = PA_12;     // エンコーダー7：疑似サーボ用エンコーダー2のA相
@@ -50,9 +52,9 @@ namespace DigitalInPins
     constexpr PinName MEASURING_ENCODER1_B      = PC_12;     // エンコーダー1：測定輪エンコーダ1のB相
     constexpr PinName MEASURING_ENCODER2_B      = PD_2 ;     // エンコーダー2：測定輪エンコーダ2のB相
 
-    constexpr PinName OMUNI_ENCODER1_B          = PC_3 ;     // エンコーダー3：オムニ用エンコーダー1のB相
-    constexpr PinName OMUNI_ENCODER2_B          = PC_1 ;     // エンコーダー4：オムニ用エンコーダー2のB相
-    constexpr PinName OMUNI_ENCODER3_B          = PB_8 ;     // エンコーダー5：オムニ用エンコーダー3のB相
+    constexpr PinName OMNI_ENCODER1_B           = PC_3 ;     // エンコーダー3：オムニ用エンコーダー1のB相
+    constexpr PinName OMNI_ENCODER2_B           = PC_1 ;     // エンコーダー4：オムニ用エンコーダー2のB相
+    constexpr PinName OMNI_ENCODER3_B           = PB_8 ;     // エンコーダー5：オムニ用エンコーダー3のB相
 
     constexpr PinName PSEUDO_SERVO_ENCODER1_B   = PC_6 ;     // エンコーダー6：疑似サーボ用エンコーダー1のB相
     constexpr PinName PSEUDO_SERVO_ENCODER2_B   = PA_11;     // エンコーダー7：疑似サーボ用エンコーダー2のB相
