3b127fa5a7ace5e25ba29737890c9416a7da0ab1
Yuya Aizu <gaku.kuro3298@gmail.com>
2025-09-02 17:27:51 +0900

    feat(Coal Arm): implement coal arm header and code

---
+++ src/Mechanism/CoalArmController.cpp
@@ -0,0 +1,96 @@
+#include "Mechanism/CoalArmController.hpp"
+
+CoalArmController::CoalArmController(PseudoServo& armServo, Servo& gripServo)
+    : _arm(armServo), _grip(gripServo), _state(State::Idle) {}
+
+void CoalArmController::init() {
+    _state = State::Init;
+    _state_timer.reset();
+    _state_timer.start();
+}
+
+void CoalArmController::collect() {
+    if (CoalArmController::isBusy()) {
+        _state = State::Collect_ArmDown;
+        _state_timer.reset();
+        _state_timer.start();
+    }
+}
+
+void CoalArmController::place() {
+    if (CoalArmController::isBusy()) {
+        _state = State::Place_ArmDown;
+        _state_timer.reset();
+        _state_timer.start();
+    }
+}
+
+bool CoalArmController::isBusy() const {
+    return _state != State::Idle && _state != State::EmergencyStop;
+}
+
+void CoalArmController::stop() {
+    _arm.home();
+    _grip.setAngleDeg(0.0f);
+    _state = State::EmergencyStop;
+    _state_timer.stop();
+}
+
+void CoalArmController::update() {
+    switch (_state) {
+        case State::Init:
+            _arm.init();
+            _grip.setAngleDeg(0.0f);
+            if (_arm.isHomed()) {
+                _state = State::Idle;
+                _state_timer.stop();
+            }
+            break;
+        case State::Collect_ArmDown:
+            _arm.setAngleDeg(-45.0f);
+            if (_arm.getAngleDeg() <= -44.0f || _state_timer.read_ms() > 3000) {
+                _state = State::Collect_GripClose;
+                _state_timer.reset();
+            }
+            break;
+        case State::Collect_GripClose:
+            _grip.setAngleDeg(45.0f);
+            if (_state_timer.read_ms() > 1000) {
+                _state = State::Collect_ArmUp;
+                _state_timer.reset();
+            }
+            break;
+        case State::Collect_ArmUp:
+            _arm.setAngleDeg(0.0f);
+            if (_arm.getAngleDeg() >= -1.0f || _state_timer.read_ms() > 3000) {
+                _state = State::Idle;
+                _state_timer.stop();
+            }
+            break;
+        case State::Place_ArmDown:
+            _arm.setAngleDeg(-45.0f);
+            if (_arm.getAngleDeg() <= -44.0f || _state_timer.read_ms() > 3000) {
+                _state = State::Place_GripOpen;
+                _state_timer.reset();
+            }
+            break;
+        case State::Place_GripOpen:
+            _grip.setAngleDeg(0.0f);
+            if (_state_timer.read_ms() > 1000) {
+                _state = State::Place_ArmUp;
+                _state_timer.reset();
+            }
+            break;
+        case State::Place_ArmUp:
+            _arm.setAngleDeg(0.0f);
+            if (_arm.getAngleDeg() >= -1.0f || _state_timer.read_ms() > 3000) {
+                _state = State::Idle;
+                _state_timer.stop();
+            }
+            break;
+        case State::EmergencyStop:
+            break;
+        default:
+            break;
+    }
+}
