cf30095fd02f64b019f98b2c1be9112542747551
5C6F2F <5c6f2f@gmail.com>
2025-09-06 22:37:33 +0900

    feat: テストコード追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include "System/WheelController/OdomWheel.hpp"
 #include "config.hpp"
 
-int main()
+int main_5()
 {
     // エンコーダーの初期化
-    Encoder encoder1(InterruptInPins::OMUNI_ENCODER1_A, DigitalInPins::OMUNI_ENCODER1_B);
-    Encoder encoder2(InterruptInPins::OMUNI_ENCODER2_A, DigitalInPins::OMUNI_ENCODER2_B);
-    Encoder encoder3(InterruptInPins::OMUNI_ENCODER3_A, DigitalInPins::OMUNI_ENCODER3_B);
+    Encoder encoder1(InterruptInPins::OMUNI_ENCODER1_A, DigitalInPins::OMUNI_ENCODER1_B, 2048, true);
+    Encoder encoder2(InterruptInPins::OMUNI_ENCODER2_A, DigitalInPins::OMUNI_ENCODER2_B, 2048, true);
+    Encoder encoder3(InterruptInPins::OMUNI_ENCODER3_A, DigitalInPins::OMUNI_ENCODER3_B, 2048, true);
     Encoder encoder4(InterruptInPins::MEASURING_ENCODER1_A, DigitalInPins::MEASURING_ENCODER1_B);
-    Encoder encoder5(InterruptInPins::MEASURING_ENCODER2_A, DigitalInPins::MEASURING_ENCODER2_B);
+    Encoder encoder5(InterruptInPins::MEASURING_ENCODER2_A, DigitalInPins::MEASURING_ENCODER2_B, 2048, true);
     std::array<Encoder *, 5> encoders = {&encoder1, &encoder2, &encoder3, &encoder4, &encoder5};
 
     // DCモーターの初期化
@@ -60,14 +60,148 @@ int main()
             .wheel_theta = M_PI},
     };
 
-    OdomWheel<5> odom_wheel(PIDGain{1, 0, 0, 100}, config, motor_controller, encoders);
+    OdomWheel<5> odom_wheel(PIDGain{25, 0, 0, 100}, config, motor_controller, encoders);
     odom_wheel.setTargetPose(Pose{0.0, 10.0, 0.0});
 
     while (true)
     {
         odom_wheel.updateTargetTwist();
-        // printf("measuring_encoder_counts: %d, %d, %d, %d, %d\n", encoder1.getCount(), encoder2.getCount(), encoder3.getCount(), encoder4.getCount(), encoder5.getCount());
+        printf("measuring_encoder_counts: %d, %d, %d, %d, %d\n", encoder1.getCount(), encoder2.getCount(), encoder3.getCount(), encoder4.getCount(), encoder5.getCount());
         printf("\n");
         wait_us(10000); // 10ms
     }
+}
+
+int main_3()
+{
+    Encoder encoder1(InterruptInPins::OMUNI_ENCODER1_A, DigitalInPins::OMUNI_ENCODER1_B, 2048, true);
+    Encoder encoder2(InterruptInPins::OMUNI_ENCODER2_A, DigitalInPins::OMUNI_ENCODER2_B, 2048, true);
+    Encoder encoder3(InterruptInPins::OMUNI_ENCODER3_A, DigitalInPins::OMUNI_ENCODER3_B, 2048, true);
+    std::array<Encoder *, 3> encoders = {&encoder1, &encoder2, &encoder3};
+
+    // PI
+    DCMotor dc1(PwmOutPins::OMUNI_MOTOR1_PWM, DigitalOutPins::OMUNI_MOTOR1_DIR);
+    // 左後方
+    DCMotor dc2(PwmOutPins::OMUNI_MOTOR2_PWM, DigitalOutPins::OMUNI_MOTOR2_DIR);
+    // 右後方
+    DCMotor dc3(PwmOutPins::OMUNI_MOTOR3_PWM, DigitalOutPins::OMUNI_MOTOR3_DIR, true);
+
+    MotorController motor1(dc1, encoder1, PIDGain({10, 0, 0, 100}));
+    MotorController motor2(dc2, encoder2, PIDGain({10, 0, 0, 100}));
+    MotorController motor3(dc3, encoder3, PIDGain({10, 0, 0, 100}));
+    std::array<MotorController *, 3> motor_controller = {&motor1, &motor2, &motor3};
+
+    // 足回り配置の設定　(ここでは駆動輪を測定輪として使用)
+    float WHEEL_RAD = 45.0;
+    float TREAD_RAD = 210.0;
+    std::array<WheelConfig, 3> config = {
+        // PI
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD, // ホイール半径
+            .wheel_x = 0.0,            // ホイールのx座標
+            .wheel_y = TREAD_RAD,      // ホイールのy座標
+            .wheel_theta = M_PI        // ホイールの向き
+        },
+        // 左後方
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD,
+            .wheel_x = (float)-M_SQRT3 / 2 * TREAD_RAD,
+            .wheel_y = (float)-0.5 * TREAD_RAD,
+            .wheel_theta = 5 * M_PI / 3},
+        // 右後方
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD,
+            .wheel_x = (float)+M_SQRT3 / 2 * TREAD_RAD,
+            .wheel_y = (float)-0.5 * TREAD_RAD,
+            .wheel_theta = M_PI / 3},
+    };
+
+    OdomWheel<3> odom_wheel(PIDGain{5, 0, 0, 100}, config, motor_controller, encoders);
+    odom_wheel.setTargetPose(Pose{0.0, 10.0, 0.0});
+
+    while (true)
+    {
+        odom_wheel.updateTargetTwist();
+        printf("measuring_encoder_counts: %d, %d, %d\n", encoder1.getCount(), encoder2.getCount(), encoder3.getCount());
+        printf("\n");
+        wait_us(10000); // 10ms
+    }
+}
+int main_2()
+{
+    // エンコーダーの初期化
+    Encoder encoder4(InterruptInPins::MEASURING_ENCODER1_A, DigitalInPins::MEASURING_ENCODER1_B);
+    Encoder encoder5(InterruptInPins::MEASURING_ENCODER2_A, DigitalInPins::MEASURING_ENCODER2_B, 2048, true);
+
+    while (true)
+    {
+        printf("measuring_encoder_counts: %d, %d\n", encoder4.getCount(), encoder5.getCount());
+        wait_us(10000); // 10ms
+    }
+}
+
+void encoder_test()
+{
+    Encoder encoder1(InterruptInPins::OMUNI_ENCODER1_A, DigitalInPins::OMUNI_ENCODER1_B, 2048, true);
+    Encoder encoder2(InterruptInPins::OMUNI_ENCODER2_A, DigitalInPins::OMUNI_ENCODER2_B, 2048, true);
+    Encoder encoder3(InterruptInPins::OMUNI_ENCODER3_A, DigitalInPins::OMUNI_ENCODER3_B, 2048, true);
+    std::array<Encoder *, 3> encoders = {&encoder1, &encoder2, &encoder3};
+
+    // PI
+    DCMotor dc1(PwmOutPins::OMUNI_MOTOR1_PWM, DigitalOutPins::OMUNI_MOTOR1_DIR);
+    // 左後方
+    DCMotor dc2(PwmOutPins::OMUNI_MOTOR2_PWM, DigitalOutPins::OMUNI_MOTOR2_DIR);
+    // 右後方
+    DCMotor dc3(PwmOutPins::OMUNI_MOTOR3_PWM, DigitalOutPins::OMUNI_MOTOR3_DIR);
+
+    MotorController motor1(dc1, encoder1, PIDGain({10, 0, 0, 100}));
+    MotorController motor2(dc2, encoder2, PIDGain({10, 0, 0, 100}));
+    MotorController motor3(dc3, encoder3, PIDGain({10, 0, 0, 100}));
+    std::array<MotorController *, 3> motor_controller = {&motor1, &motor2, &motor3};
+
+    // 足回り配置の設定　(ここでは駆動輪を測定輪として使用)
+    float WHEEL_RAD = 45.0;
+    float TREAD_RAD = 210.0;
+    std::array<WheelConfig, 3> config = {
+        // PI
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD, // ホイール半径
+            .wheel_x = 0.0,            // ホイールのx座標
+            .wheel_y = TREAD_RAD,      // ホイールのy座標
+            .wheel_theta = M_PI        // ホイールの向き
+        },
+        // 左後方
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD,
+            .wheel_x = (float)-M_SQRT3 / 2 * TREAD_RAD,
+            .wheel_y = -TREAD_RAD / 2,
+            .wheel_theta = 5 * M_PI / 3},
+        // 右後方
+        WheelConfig{
+            .wheel_radius = WHEEL_RAD,
+            .wheel_x = (float)+M_SQRT3 / 2 * TREAD_RAD,
+            .wheel_y = -TREAD_RAD / 2,
+            .wheel_theta = M_PI / 3},
+    };
+
+    OdomWheel<3> odom_wheel(PIDGain{25, 0, 0, 100}, config, motor_controller, encoders);
+    odom_wheel.setTargetPose(Pose{0.0, 10.0, 0.0});
+
+    while (true)
+    {
+        encoder2.addCount(5);
+        encoder3.addCount(5);
+        auto pos = odom_wheel.odometry.getCurrentPose();
+        printf("%f, %f, %f\n", pos.x, pos.y, pos.theta);
+        printf("measuring_encoder_counts: %d, %d, %d\n", encoder1.getCount(), encoder2.getCount(), encoder3.getCount());
+        wait_us(10000); // 10ms
+    }
+}
+
+int main()
+{
+    // main_5();
+    main_3();
+    // main_2();
+    // encoder_test();
 }
